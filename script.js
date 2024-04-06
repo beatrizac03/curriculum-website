ba4ba40088dc80c77de9b3f304088a267190f045
beatrizac03 <beatrizaguiarc14@gmail.com>
2024-04-05 21:53:42 -0300

    updates 1.5

--- script.js
+++ script.js
@@ -15,13 +15,29 @@ window.addEventListener('scroll', toggleBlur)
 
 /* dash below li link*/
 
+// let eachLiLink = document.querySelectorAll('.c-links a')
+
+// function toggleDashBelow(event){
+//     let liLink = event.currentTarget.querySelector('li');
+//     liLink.classList.toggle('afterDashLink')
+// }
+
+// eachLiLink.forEach((link) => {
+//     link.addEventListener('mouseover', toggleDashBelow)
+// })
+
 let eachLiLink = document.querySelectorAll('.c-links a')
 
-function toggleDashBelow(event){
-    let liLink = event.currentTarget.querySelector('li');
-    liLink.classList.toggle('afterDashLink')
-}
-
-eachLiLink.forEach((link) => {
-    link.addEventListener('mouseover', toggleDashBelow)
+eachLiLink.forEach((eachLi) => {
+    eachLi.addEventListener('mouseenter', function() {
+        eachLi.classList.add('afterLiDash')
+    })
 })
+
+eachLiLink.forEach((eachLi) => {
+    eachLi.addEventListener('mouseleave', function() {
+        eachLi.classList.remove('afterLiDash')
+    })
+})
+
+
